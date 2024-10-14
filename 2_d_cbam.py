accbf9e77cb7e556ebce9d0c2af0d6966c48ca4e
Platohykj <abc181891aa@outlook.com>
2024-10-14 16:34:47 +0800

    Add CBAM

--- 2_d_cbam.py
+++ 2_d_cbam.py
@@ -103,10 +103,10 @@ def main():
     ])
 
     # 加载CIFAR-10数据集
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 
     # 定义DenseNet模型并加入CBAM
